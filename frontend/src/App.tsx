b22e4d78786719e84487d76d28a945cc78fc72b9
Ashok Chaudhari <92539504+AshokChaudhari07@users.noreply.github.com>
2025-05-05 03:19:33 +0530

    feat: add header and navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import Header from "components/Header";
+import Navbar from "components/Navbar";
 import ProductList from "components/ProductList";
 import products from "data";
 
 const App = () => {
   return (
     <>
+      <Navbar />
+      <Header />
       <ProductList products={products} />
     </>
   );
